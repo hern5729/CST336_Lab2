d73dbf6c47f8e828b598c3bce62537e4b42b8cd1
dean1509 <bdeanda@csumb.edu>
2015-02-11 19:26:50 +0000

    added checkDuplicate function to function file

--- includes/functions.php
+++ includes/functions.php
@@ -1,6 +1,24 @@
 <?php
 
-/*
- * To change this template use Tools | Templates.
- */
+function generateRandomSuit(){
+    
+    $suit = rand(1,4);
+    switch($suit){
+        case 1: $folder = "clubs";
+        break;
+        case 2: $folder = "diamonds";
+        break;
+        case 3: $folder = "hearts";
+        break;
+        case 4: $folder = "spades";
+        break;
+    }
+    return $folder;
+}
+
+function checkDuplicates($array, $valueToCompare){
+    for($index = 0; index < count($array); $index++){
+        if(i)
+    }
+}
 ?>
